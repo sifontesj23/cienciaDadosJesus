c584024b47bc8bac3c9d12575cd5b01e201f1dfc
sifontesj23 <sifontesj231276@gmail.com>
2024-12-16 20:00:31 -0300

    teste un e inicio test 2

--- projeto4/aula-sifontesj23/src/app.py
+++ projeto4/aula-sifontesj23/src/app.py
@@ -20,59 +20,62 @@ except Exception as e:
     st.stop()
 
 # Selecionando colunas categóricas
-#quali = [ 'REGIAO_BENEFICIARIO_BOLSA','RACA_BENEFICIARIO_BOLSA']
-#quali2 = [ 'REGIAO_BENEFICIARIO_BOLSA','RACA_BENEFICIARIO_BOLSA','SEXO_BENEFICIARIO_BOLSA']
+quali = [ 'REGIAO_BENEFICIARIO_BOLSA','RACA_BENEFICIARIO_BOLSA']
+
 
 
 # Transformando colunas categóricas em dummies
-#PROUNI_dummies = pd.get_dummies(PROUNI[quali])
-#PROUNI_dummies2 = pd.get_dummies(PROUNI[quali2])
+PROUNI_dummies = pd.get_dummies(PROUNI[quali])
 
-#st.write("Colunas transformadas em variáveis dummy:", quali)
-#st.dataframe(PROUNI_dummies.head())
-#st.write("Colunas transformadas em variáveis dummy:", quali2)
-#st.dataframe(PROUNI_dummies2.head())
 
-# Aplicando o algoritmo K-Means
-#x = PROUNI_dummies.values
-#kmeans = KMeans(n_clusters=2, random_state=42)
-#kmeans.fit(x)
+st.write("Colunas transformadas em variáveis dummy:", quali)
+st.dataframe(PROUNI_dummies.head())
+
+#Aplicando o algoritmo K-Means
+st.write("Aplicando o algoritmo K-Means")
+x = PROUNI_dummies.values
+kmeans = KMeans(n_clusters=2, random_state=42)
+kmeans.fit(x)
 #x2 = PROUNI_dummies2.values
 #kmeans = KMeans(n_clusters=5, random_state=42)
 #kmeans.fit(x2)
  
 
 # Plotando os clusters com os dois primeiros atributos
-#fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 
 
-#plt.scatter(# plota os pontos
- #   x[:, 0], x[:, 1], #x e y
-  #  c = kmeans.labels_, #cores de acordo com o grupo
-   # marker='o', # pontos com 'o'
-    #edgecolor='black', #cor da borda
-    #cmap='rainbow', #esquema de cores
-    #s=100 #tamanho
-#)
+plt.scatter(# plota os pontos
+    x[:, 0], x[:, 1], #x e y
+    c = kmeans.labels_, #cores de acordo com o grupo
+   marker='o', # pontos com 'o'
+    edgecolor='black', #cor da borda
+    cmap='rainbow', #esquema de cores
+    s=100 #tamanho
+    )
 # Adicionando os centróides
 
-#plt.scatter(# plota os centróides
- #   kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:, 1],#x e y
-  #  c='white',  #cor fixa
-   # marker='*', #pontos marcados com estrela
-    #edgecolor='black', #cor da borda
-    #s=100 #tamanho
-#)
+plt.scatter(# plota os centróides
+     kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:, 1],#x e y
+   c='white',  #cor fixa
+    marker='*', #pontos marcados com estrela
+    edgecolor='black', #cor da borda
+    s=100 #tamanho
+)
 
 #ax.set_title("Clusterização com K-Means (usando os dois primeiros atributos)")
 #ax.legend()
-#st.pyplot(fig)
+st.pyplot(fig)
 
 
-#################################################################################
+###################SEGUNDO TESTE##############################################################
+
 st.header("Segundo teste, usando mais atributos")
 
-
+quali2 = ['NOME_IES_BOLSA', 'TIPO_BOLSA','MODALIDADE_ENSINO_BOLSA','NOME_CURSO_BOLSA','NOME_TURNO_CURSO_BOLSA','SEXO_BENEFICIARIO_BOLSA','RACA_BENEFICIARIO_BOLSA','BENEFICIARIO_DEFICIENTE_FISICO','REGIAO_BENEFICIARIO_BOLSA','SIGLA_UF_BENEFICIARIO_BOLSA','MUNICIPIO_BENEFICIARIO_BOLSA']
+PROUNI_dummies2 = pd.get_dummies(PROUNI[quali2])
+st.write("Colunas transformadas em variáveis dummy:")
+st.dataframe(PROUNI_dummies2.head())
 
 
 
